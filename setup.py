fffa21d7f9fae44138cae232f9ef2825e2e668df
Leonid Belyi <leonid.belyi@bitsighttech.com>
2019-12-17 10:24:53 -0500

    Change version string to reflect existing bs1 convention

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from os.path import join, dirname
 from setuptools import setup, find_packages
 import sys
 
-VERSION = (7, 0, 5, "bs1")
+VERSION = (7, 0, 5)
 __version__ = VERSION
-__versionstr__ = ".".join(map(str, VERSION))
+__versionstr__ = ".".join(map(str, VERSION)) + "bs1"
 
 f = open(join(dirname(__file__), "README"))
 long_description = f.read().strip()
